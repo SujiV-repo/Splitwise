1ec7fadd05bce87ad7b33b96ca16534d0641c3f8
Sujith Veturi <veturisujith1999@gmail.com>
2023-11-02 13:22:02 +0530

    settleup api call and creating controller

--- src/main/java/com/project/splitwise/controller/GroupController.java
+++ src/main/java/com/project/splitwise/controller/GroupController.java
@@ -1,7 +1,24 @@
 package com.project.splitwise.controller;
 
+import com.project.splitwise.dto.TransactionDTO;
+import com.project.splitwise.exception.GroupNotFoundException;
+import com.project.splitwise.service.GroupService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class GroupController {
+    @Autowired
+    private GroupService groupService;
+
+    @GetMapping("/settleup/{groupId}")
+    public ResponseEntity settleUpForGroup(@PathVariable("groupId") int groupId) throws GroupNotFoundException {
+        List<TransactionDTO> transactions = groupService.settleUpByGroupId(groupId);
+        return ResponseEntity.ok(transactions);
+    }
 }
